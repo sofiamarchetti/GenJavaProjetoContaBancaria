f53559ee2aea68d8c06337a46128175fb01fbfb3
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-05-02 19:30:01 -0300

    atualização menu

--- conta/Menu.java
+++ conta/Menu.java
@@ -2,33 +2,38 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.controller.ContaController;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
 
-	public static Scanner leia = new Scanner(System.in);
-	
 	public static void main(String[] args) {
-
-		int opcao;
-
-		// Teste da Classe Conta Corrente
-		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
 		
-        // Teste da Classe Conta Poupança
-		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
-		cp1.visualizar();
-        cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
+		ContaController contas = new ContaController();
+		
+		Scanner leia = new Scanner(System.in);
+
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		
+		System.out.println("\nCriar contas\n");
+		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
+		contas.cadastrar(cc1);
+		
+		ContaCorrente cc2 = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000f, 100.0f);
+		contas.cadastrar(cc2);
+		
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000f, 12);
+		contas.cadastrar(cp1);
+		
+		ContaPoupanca cp2 = new ContaPoupanca(contas.gerarNumero(), 126, 2, "Juliana Ramos", 8000f, 15);
+		contas.cadastrar(cp2);
+		
+		contas.listarTodas();
 		
 		while (true) {
 			
@@ -40,7 +45,7 @@ public class Menu {
 			System.out.println("                                                     ");
 			System.out.println("            1 - Criar Conta                          ");
 			System.out.println("            2 - Listar todas as Contas               ");
-			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            3 - Buscar Conta por Número              ");
 			System.out.println("            4 - Atualizar Dados da Conta             ");
 			System.out.println("            5 - Apagar Conta                         ");
 			System.out.println("            6 - Sacar                                ");
@@ -61,33 +66,64 @@ public class Menu {
 			
 			switch(opcao) {
 			case 1:
-				System.out.println("\n Criar Conta");
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
 				
-                 break;
+				System.out.println("Digite o número da agência: ");
+				agencia = leia.nextInt();
+				System.out.println("Digite o nome do titular: ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+				
+				do {
+					System.out.println("Digite o tipo da conta (1-CC ou 2-CP): ");
+					tipo = leia.nextInt();
+				}while(tipo < 1 && tipo >2);
+				
+				System.out.println("Digite o saldo da conta (R$): ");
+				saldo = leia.nextFloat();
+				
+				switch(tipo) {
+					case 1 -> {
+						System.out.println("Digite o limite de crédito (R$): ");
+						limite = leia.nextFloat();
+						contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));						
+					}
+					case 2 -> {
+						System.out.println("Digite o dia do aniversário da conta: ");
+						aniversario = leia.nextInt();
+						contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+					}
+				}
+				
+				keyPress();
+                break;
+                 
 			case 2:
 				System.out.println("\n Listar todas as Contas");
-				
-                 break;
+				contas.listarTodas();
+				keyPress();
+                break;
+                 
 			case 3:
-				System.out.println("\n Buscar Conta por número");
-				
+				System.out.println("\n Buscar Conta por número");				
 				break;
+				
 			case 4:
-				System.out.println("\n Atualizar dados da Conta");
-				
+				System.out.println("\n Atualizar dados da Conta");				
                  break;
+                 
 			case 5:
-				System.out.println("\n Apagar Conta");
-				
+				System.out.println("\n Apagar Conta");				
                  break;
+                 
 			case 6:
-				System.out.println("\n Sacar");
-				
+				System.out.println("\n Sacar");				
 				break;
+				
              case 7:
-				System.out.println("\n Depositar");
-				
+				System.out.println("\n Depositar");				
 				break;
+				
              case 8:
 				System.out.println("\n Transferir");
 				
@@ -97,6 +133,11 @@ public class Menu {
                  break;
 			}
         }
+	}
+
+	private static void keyPress() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
