76bcf3f16c8e38625138bcc79cea85cbdf1753da
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-05-03 20:40:46 -0300

    Update ContaController.java

--- conta/controller/ContaController.java
+++ conta/controller/ContaController.java
@@ -11,7 +11,12 @@ public class ContaController implements ContaRepository{
 
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub		
+		var conta = buscarNaCollection(numero);
+		
+		if (conta != null)
+			conta.visualizar();
+		else
+			System.out.println("\nA conta número: "+numero+" não foi encontrada!");
 	}
 
 	@Override
@@ -29,13 +34,24 @@ public class ContaController implements ContaRepository{
 
 	@Override
 	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub		
+		var buscaConta = buscarNaCollection (conta.getNumero());
+		
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("\nA conta número: "+conta.getNumero()+" foi atualizada com sucesso!");
+		}else
+			System.out.println("\nA conta número: "+conta.getNumero()+" não foi encontrada!");
 	}
 
 	@Override
 	public void deletar(int numero) {
-		// TODO Auto-generated method stub		
-	}
+		var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			if (listaContas.remove(conta) == true)
+				System.out.println("\nA conta número: "+numero+" foi deletada com sucesso!");
+		}else
+			System.out.println("\nA conta número: "+numero+" não foi encontrada!");
 
 	@Override
 	public void sacar(int numero, float valor) {
@@ -55,5 +71,22 @@ public class ContaController implements ContaRepository{
 	public int gerarNumero() {
 		return ++ numero;
 	}
-
+	
+	public Conta buscarNaCollection (int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		
+		return null;
+	}
+	
+	public int retornaTipo(int numero) {
+		for (var conta : listaContas) {
+			return conta.getTipo();
+		}
+	}
+	return 0;
+}
 }
